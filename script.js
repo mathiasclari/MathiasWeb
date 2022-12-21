f5d8a759c4d5b0cfe2bd378e81158da7462fb2f2
Mathias <pxlib@proton.me>
2022-12-21 01:32:10 +0100

    Updated

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 
-links.forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const href = link.getAttribute('href');
-        const offsetTop = document.querySelector(href).offsetTop;
-
-        window.scrollTo({
-            top: offsetTop,
-            behavior: 'smooth'
-        });
-    });
-});
 
 
 
